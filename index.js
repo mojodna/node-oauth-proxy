b226076b64585fe8e56947c298d80e3ba46d1b9a
Seth Fitzsimmons <seth@mojodna.net>
2013-09-28 15:24:31 -0700

    Preliminary HTTPS support
    Instead of tunneling the socket provided on the `connect` event to the
    target server, connect to a local HTTPS server and present a self-signed
    certificate (ideally one that matches the target hostname and is signed
    with a CA that's been installed--check mitmproxy to see how this is done
    dynamically) before proxying the request normally.
    
    This will allow the `ssl` option to be deprecated and for proxy clients
    to make HTTPS connections directly and have them signed.

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ module.exports = function(options) {
     console.warn("Warning: consumer key and/or secret are empty--expect the unexpected.");
   }
 
-  var server = http.createServer(function(req, res) {
+  var proxyHandler = function(req, res) {
     // TODO are there other headers that should be dropped?
     // how are Connection headers supposed to be handled?
     delete req.headers["proxy-connection"];
@@ -28,6 +28,19 @@ module.exports = function(options) {
     // TODO add X-Forwarded-For headers
 
     var target = url.parse(req.url);
+
+    if (!target.host) {
+      // the server is not operating in proxy mode (req.url does not contain
+      // a full URL)
+      // TODO check this against a server instance
+      target.host = req.headers.host;
+      target.hostname = req.headers.host;
+
+      if (this._pipeName) {
+        // our HTTPS server doesn't listen on a port, so this is tunneled HTTPS
+        target.protocol = "https:";
+      }
+    }
 
     if (options.ssl) {
       target.protocol = "https:";
@@ -53,15 +66,23 @@ module.exports = function(options) {
 
     req.pipe(upstream);
     upstream.pipe(res);
-  });
+  };
+
+  var server = http.createServer(proxyHandler);
+  var fs = require("fs");
+  var https = require("https").createServer({
+    key: fs.readFileSync("agent2-key.pem"),
+    cert: fs.readFileSync("agent2-cert.pem")
+  }, proxyHandler);
+
+  https.listen("/tmp/oauth-proxy-https.sock");
 
   server.on("connect", function(req, socket, head) {
-    console.warn("CONNECT method in use; requests will not be signed.");
-
-    var parts = req.url.split(":", 2);
-
-    // TODO connect to myself and present a self-signed certificate?
-    var conn = net.connect(parts[1], parts[0], function() {
+    // connect to myself and present a self-signed certificate
+    // this uses a pipe for 2 reasons:
+    // 1) no need to make it public
+    // 2) we can detect when we're tunneling and set protocol appropriately
+    var conn = net.connect("/tmp/oauth-proxy-https.sock", function() {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
 
       socket.pipe(conn);
